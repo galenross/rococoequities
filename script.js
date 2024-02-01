d7917111018ab01f20696e3fe6391f7374151584
galenross <151068520+galenross@users.noreply.github.com>
2024-02-01 18:59:17 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ function calculateCompoundInterest() {
     const interestRate = parseFloat(document.getElementById('interestRate').value);
     const initialAmount = parseFloat(document.getElementById('initialAmount').value);
 
-        var theAnswer = initialAmount * interestRate
+        var theAnswer = (initialAmount * interestRate
         +initialAmount*interestRate
-        -(initialAmount * 0.25);
+        -(initialAmount * 0.25)).val;
 
     const resultAmountElement = document.getElementById('resultAmount');
-    resultAmountElement.textContent = theAnswer.toFixed(2);
+    resultAmountElement.textContent = theAnswer;
     
 
 }
